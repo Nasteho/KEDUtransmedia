16b9f5d1cdd2a3cf1ae38878fef36304c304f47b
Nasteho <Nasteho@users.noreply.github.com>
2018-04-04 14:04:32 +0200

    xdfcgvhbjnkml,

---
+++ assets/javascript/script.js
@@ -0,0 +1,7 @@
+function showKollega() {
+  if (kollegacontent.style.display == 'block') {
+    kollegacontent.style.display = 'none';
+    differentform.style.display = 'none';
+    kedu.style.display = 'none';
+    }
+  }
