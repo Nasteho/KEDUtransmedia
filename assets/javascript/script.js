721c274134d01b4905dbf538d73f2e744721bf80
Nasteho <Nasteho@users.noreply.github.com>
2018-04-09 23:48:32 +0200

    d

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -38,23 +38,33 @@ for(var i = 0; i <= 12; i++) {
   for(var j = 0; j < radios.length; j++) {
     var radio = radios[j];
     if(radio.value == "true" && radio.checked) {
-      score++;
+      score++; /* tilføjer én til score hvis den valgte radio button og radiobuttons matcher*/
     }
   }
 }
-        document.getElementById("resultsAnswers").innerHTML = "Correct Responses: " + score;
+        document.getElementById("resultsAnswers").innerHTML = "Korrekte resultater: " + score + " / 4";
         document.getElementById("resultsAnswers").style.color = "green";
         document.getElementById("submit").style.display = 'none';
         document.getElementById("nextstep").style.display = 'block';
-        document.getElementById("nextstep").style.backgroundColor = "red";
+        document.getElementById("nextstep").style.backgroundColor = "white";
+        document.getElementById("nextstep").style.color = "#00a19b";
 
       }
 
-/*---------- */
+/* ---------- */
 
 
 
 function hideThisShowSecond() {
   kedupart.style.display = 'none';
   skattejagtquiz.style.display = 'block';
+  finalstep.style.display = 'none';
 }
+
+function showFinalStep() {
+  kedupart.style.display = 'none';
+  skattejagtquiz.style.display = 'none';
+  finalstep.style.display = 'block';
+}
+
+/* forum local storage kode */
