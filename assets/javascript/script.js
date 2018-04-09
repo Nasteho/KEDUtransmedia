f2c5ecce037279736a98210aaec7bd5682deea73
Nasteho <Nasteho@users.noreply.github.com>
2018-04-09 08:23:07 +0200

    ss

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -28,3 +28,25 @@ function hideKollega () {
 function hideDifferentForm() {
   differentformcontent.style.display = 'none';
 }
+
+
+/*-------- transmedia se resultater -------*/
+var answers = ["1","3","2","1"],
+    tot = answers.length;
+
+function getCheckedValue( radioName ){
+    var radios = document.getElementsByName("radioName"); // Get radio group by-name
+    for(var y=0; y<radios.length; y++)
+      if(radios[y].checked) return radios[y].value; // return the checked value
+}
+
+function getScore(){
+  var score = 0;
+  for (var i=0; i<tot; i++)
+    if(getCheckedValue("question"+i)===answers[i]) score += 1; // increment only
+  return score;
+}
+
+function returnScore(){
+  alert("Your score is "+ getScore() +"/"+ tot);
+}
