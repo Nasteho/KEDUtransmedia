cbf479b29da69d252a53ac1c271b580d7b9203e2
Nasteho <Nasteho@users.noreply.github.com>
2018-04-17 08:57:26 +0200

    i

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -40,18 +40,33 @@ for(var i = 0; i <= 12; i++) {
     if(radio.value == "true" && radio.checked) {
       score++; /* tilføjer én til score hvis den valgte radio button og radiobuttons matcher*/
     }
+    if (score >= 3) {
+      document.getElementById("svar").innerHTML = "Tillykke du har hjulpet Malene med at huske igen! Tryk næste for at se hvad hun har at fortælle";
+      svar.style.color = "green";
+      submit.style.display = 'none';
+      nextstep.style.display = 'block';
+      document.getElementById("resultsAnswers").style.color = "green";
+    } else {
+      document.getElementById("svar").innerHTML = "Malene kan stadig ikke huske ikke, tag testen igen for at hjælpe hende";
+      document.getElementById("resultsAnswers").style.color = "red";
+      document.getElementById("resultsAnswers").style.fontWeight = "700";
+      svar.style.color = "red";
+      submit.style.display = 'block';
+      nextstep.style.display = 'none';
+
+    }
   }
 }
         document.getElementById("resultsAnswers").innerHTML = "Korrekte resultater: " + score + " / 4";
-        document.getElementById("resultsAnswers").style.color = "green";
-        document.getElementById("submit").style.display = 'none';
-        document.getElementById("nextstep").style.display = 'block';
+
         document.getElementById("nextstep").style.backgroundColor = "white";
         document.getElementById("nextstep").style.color = "#00a19b";
 
       }
 
 /* ---------- */
+
+
 
 
 
@@ -66,5 +81,3 @@ function showFinalStep() {
   skattejagtquiz.style.display = 'none';
   finalstep.style.display = 'block';
 }
-
-/* forum local storage kode */
